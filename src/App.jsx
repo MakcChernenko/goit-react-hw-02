91628795c0372b21bb543ba4bc2838da92a2992f
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2024-08-08 15:30:11 +0300

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Description from "./components/Description/Description";
 import Notification from "./components/Notification/Notification";
 
 function App() {
+  // тут логіка зміни і збереження у локальному хранилищі feedback-ка
   const [feedback, setFeedback] = useState(() => {
     const savedFeedback = window.localStorage.getItem("feedback");
     if (savedFeedback !== null) {
@@ -19,32 +20,37 @@ function App() {
       };
     }
   });
-  const [statistick, setStatistick] = useState(0);
-  useEffect(() => {
-    setStatistick(
-      Math.round((feedback.good / (feedback.good + feedback.bad)) * 100)
-    );
-  }, [statistick, feedback]);
-  const resetFeedback = () => {
-    setFeedback({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-    window.localStorage.removeItem("feedback");
-  };
-
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
       [feedbackType]: prevFeedback[feedbackType] + 1,
     }));
   };
-
   useEffect(() => {
     window.localStorage.setItem("feedback", JSON.stringify(feedback));
   }, [feedback]);
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  // тут просто збираю загальну кількість кліків і через useEffect змушую елементи
+  // залежні від totalFeedback змінюватися
+  const [totalFeedback, setTotalFeedback] = useState(0);
+  useEffect(() => {
+    setTotalFeedback(feedback.good + feedback.neutral + feedback.bad);
+  }, [feedback]);
+  // тут вираховую відсоток позитивних відгуків і компонент із prop-ом statistick змінюється
+  const [statistick, setStatistick] = useState(0);
+  useEffect(() => {
+    setStatistick(
+      Math.round((feedback.good / (totalFeedback - feedback.neutral)) * 100)
+    );
+  }, [feedback, totalFeedback]);
+  // тут обнуляю feedback, хоча можливо це можна б було зробити через useEffect
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+    window.localStorage.setItem("feedback", feedback);
+  };
 
   return (
     <div className={clsx(css.app)}>
@@ -64,7 +70,7 @@ function App() {
           statistick={statistick}
         />
       )}
-      {totalFeedback === 0 && <Notification />}
+      {!totalFeedback && <Notification />}
     </div>
   );
 }
